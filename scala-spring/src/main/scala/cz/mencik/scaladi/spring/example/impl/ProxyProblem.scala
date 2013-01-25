5b89962a7b6a293463551dbd8c5fb2be959a1af9
Vlastimil Mencik <v_mencik@hotmail.com>
2013-01-25 21:03:33 +0100

    Modified the proxy problem example.

--- scala-spring/src/main/scala/cz/mencik/scaladi/spring/example/impl/ProxyProblem.scala
+++ scala-spring/src/main/scala/cz/mencik/scaladi/spring/example/impl/ProxyProblem.scala
@@ -1,15 +1,21 @@
 package cz.mencik.scaladi.spring.example.impl
+import java.util.logging.Logger
+import java.util.logging.Level
 
 class AuthorizedUser {
 
   // in the compiled bytecode there is a final method created
-  // for this field ant this class cannot be proxied
-  private val userId: Int = 15
+  // for this field and this class cannot be proxied
+  private val logger = Logger.getLogger("my.logger")
 
-  def someMethod: Seq[Int] = {
-    val numbers = 1 to 100
-    // if we remove the use of the field in the closure,
-    // the final method will not be emitted
-    numbers filter { _ % 2 == 0 }
+  def someMethod: Unit = {
+    val colors = List("Red", "Green", "Blue")
+
+    colors foreach { c =>
+      // if we remove the use of the field in the closure,
+      // the final method will not be emitted
+      logger.log(Level.INFO, c)
+    }
   }
+
 }
